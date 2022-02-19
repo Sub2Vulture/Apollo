3d3a701e88b842bf7351227dbfd9583bd63b0684
Sub2Vulture <itsvulturemain@gmaail.com>
2022-02-20 00:09:48 +0530

    #2.1 commit (cmd)

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -7,6 +7,30 @@ module.exports = {
   permissions: [],
   devOnly: false,
   run: async ({ client, message, args }) => {
-    message.reply("Pong!");
+    message.reply("**Pinging...**").then((resultMessage) => {
+      const ping = resultMessage.createdTimestamp - message.createdTimestamp;
+
+      const pingEmbed = new MessageEmbed()
+        .setColor("#303136")
+        .setTitle("Pong!")
+        .setDescription(
+          "<:Icon_ping:943155157833744424> Average websocket latency: `" +
+            ping +
+            " ms`"
+        )
+        .setFields(
+          {
+            name: "**API**",
+            value: "`" + client.ws.ping + "` ms",
+            inline: true,
+          },
+          {
+            name: "**Database**",
+            value: "`None`",
+            inline: true,
+          }
+        );
+      resultMessage.edit({ content: null, embeds: [pingEmbed] });
+    });
   },
 };
