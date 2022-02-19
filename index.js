2caf6b7d1baaac849b6f335bf2d4dd525bb789a2
Sub2Vulture <itsvulturemain@gmaail.com>
2022-02-20 00:00:57 +0530

    #2.1 commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  allowedMentions: [{ repliedUser: false }],
 });
 
 let bot = {
